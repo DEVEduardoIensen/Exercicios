d12591767dd24821b0d2953f2fc5998a1b4ea0ac
DEVEeduardoiensen <eduardo.iensen.prestes@escola.pr.gov.br>
2024-02-06 12:45:09 -0300

    aula2

---
+++ scripts/TiposDeVariaveis.js
@@ -0,0 +1,48 @@
+// TIPOS DE VARIAVEIS
+
+// string
+
+let nome = "joao";
+
+// inteiros  
+
+let numero = 10;
+// float ( decimais)
+
+let decimal = 3.14;
+
+
+// lista   ( array)
+
+
+
+
+
+var  lista = ["ovo","coca","cafe",];
+
+alert (lista[])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ scripts/sistema.js
@@ -0,0 +1,34 @@
+
+console.log("capa");
+//VARIÁVEIS
+// local
+let lestgo = 10;
+//globais
+
+var terca = 4;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+alert("O RESULTADO DE "+lestgo+" MENOS "+terca+" É: "+ (lestgo-terca));
+alert("O RESULTADO DE "+lestgo+" X "+terca+" É: "+ (lestgo*terca));
+alert("O RESULTADO DE "+lestgo+" DIVIDIDO "+terca+" É: "+ (lestgo/terca));
+alert("O RESULTADO DE "+lestgo+" mais "+terca+" É: "+ (lestgo+terca));
