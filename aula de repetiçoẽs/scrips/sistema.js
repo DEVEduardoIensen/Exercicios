e612ab8661c1be3284ac5a6ad9106ecb4686081c
DEVEeduardoiensen <eduardo.iensen.prestes@escola.pr.gov.br>
2024-02-07 09:12:35 -0300

    aula2

---
+++ aula de repetiçoẽs/scrips/sistema.js
@@ -0,0 +1,10 @@
+
+const NUMERO = 19;
+
+if(NUMERO==20){
+    alert(NUMERO/10);
+}else {alert(" o numero  é  menor que 20")
+    
+}
+
+
---
+++ scripts/for.js
@@ -0,0 +1,7 @@
+var inicio = 5;
+var final = 12;
+
+for ( inicio; inicio  < final; inicio++){
+    alert(inicio)
+    console.log(inicio);
+}
