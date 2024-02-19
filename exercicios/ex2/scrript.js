ab9771a9cf2a30e0a3d752f9891d853829eaaa18
Professor João Paulo <125033758+ProfJoaoPaulox@users.noreply.github.com>
2024-02-19 08:57:03 -0300

    Professor: Correção do Algoritmo

--- exercicios/ex2/scrript.js
+++ exercicios/ex2/scrript.js
@@ -1,3 +1,3 @@
-for (let numero = 1; numero <= 20; numero += 1) {
+for (let numero = 1; numero <= 20; numero += 2) { //Deve-se somar 2, pois o exercício requer apenas os pares
     console.log(numero);
 }
